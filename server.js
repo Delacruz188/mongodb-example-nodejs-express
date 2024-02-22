a4b1fe994355ba4aa595ac3ec46cf75b337bc2c1
CDelacruz1802 <carlos.de.la.cruz@accenture.com>
2024-02-22 14:03:24 -0800

    Clase 1 [extra metodos]

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Importa las dependencias
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Configura la aplicación Express
 const app = express();
 app.use(express.json());
 
 // Conexión a la base de datos MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/Escuela'); //! <------------ IMPORTANTE
+mongoose.connect(process.env.MONGODB_URI); //! <------------ IMPORTANTE
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
 db.once('open', () => {
@@ -35,6 +36,41 @@ app.get('/usuarios', async (req, res) => {
   }
 });
 
+// Ruta POST para crear un nuevo usuario
+app.post('/usuarios', async (req, res) => {
+  try {
+    const nuevoUsuario = new Usuario(req.body);
+    await nuevoUsuario.save();
+    res.status(201).json(nuevoUsuario);
+  } catch (error) {
+    console.error('Error al crear usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Ruta PUT para actualizar un usuario existente
+app.put('/usuarios/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(id, req.body, { new: true });
+    res.json(usuarioActualizado);
+  } catch (error) {
+    console.error('Error al actualizar usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Ruta DELETE para eliminar un usuario
+app.delete('/usuarios/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Usuario.findByIdAndDelete(id);
+    res.json({ message: 'Usuario eliminado correctamente' });
+  } catch (error) {
+    console.error('Error al eliminar usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
 
 
 // Inicia el servidor
