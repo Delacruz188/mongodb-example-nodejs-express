82fda3f86a459d4a03cf9a6b7d05549b940c52af
Carlos De La Cruz <64759617+Delacruz188@users.noreply.github.com>
2024-03-14 19:21:06 -0600

    Create clase0314Example.jsx

---
+++ clase0314Example.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import logo from "../public/logo.svg";
+
+function App() {
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <div style={{ display: "flex" }}>
+          <div>
+            {/ //* <img src={logo} alt="" /> */}
+          </div>
+          <div style={{ marginLeft: "50px" }}>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
